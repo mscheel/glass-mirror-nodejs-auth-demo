2334718cc3a9155eac37f35e5cfb710ebf57aa08
Mark Scheel <mscheel@digitalconstruction.com>
2014-05-04 10:41:59 -0600

    app.js changes, reducing feature scope, add environment variable setting hints, small text customizations

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-/**
- * Module dependencies.
- */
+// Module dependencies
 
-var express = require('express')
-    , http = require('http')
-    , googleapis = require('googleapis')
-    , OAuth2Client = googleapis.OAuth2Client;
+var express = require('express'), http = require('http'), googleapis = require('googleapis'), OAuth2Client = googleapis.OAuth2Client;
 
-// Use environment variables to configure oauth client.
-// That way, you never need to ship these values, or worry
-// about accidentally committing them
-var oauth2Client = new OAuth2Client(process.env.MIRROR_DEMO_CLIENT_ID,
-    process.env.MIRROR_DEMO_CLIENT_SECRET, process.env.MIRROR_DEMO_REDIRECT_URL);
+//You will need to set these three environment variables before running
+//Find the values at the Developer Console APIs & Auth > APIs > Credentials > OAuth
+//Mac: $ EXPORT PROJECT_NAME_CLIENT_ID="1482blahblah"
+//PC:  $ EXPORT PROJECT_NAME_CLIENT_ID 1482blahblah
+//Open new shell prompt to be sure 
+
+var oauth2Client = new OAuth2Client(process.env.SDFGGXE16_CLIENT_ID,
+		process.env.SDFGGXE16_CLIENT_SECRET,
+		process.env.SDFGGXE16_REDIRECT_URI);
 
 var app = express();
 
 // all environments
-app.set('port', 8081);
+app.set('port', 3000);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -24,161 +23,102 @@ app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
-var success = function (data) {
-    console.log('success', data);
+var success = function(data) {
+	console.log('success', data);
 };
-var failure = function (data) {
-    console.log('failure', data);
+var failure = function(data) {
+	console.log('failure', data);
 };
-var gotToken = function () {
-    googleapis
-        .discover('mirror', 'v1')
-        .execute(function (err, client) {
-            if (!!err) {
-                failure();
-                return;
-            }
-            console.log('mirror client', client);
-            listTimeline(client, failure, success);
-            insertHello(client, failure, success);
-            insertContact(client, failure, success);
-            insertLocation(client, failure, success);
-        });
+var gotToken = function() {
+	googleapis.discover('mirror', 'v1').execute(function(err, client) {
+		if (!!err) {
+			failure();
+			return;
+		}
+		console.log('mirror client', client);
+		listTimeline(client, failure, success);
+		insertHello(client, failure, success);
+	});
 };
 
 // send a simple 'hello world' timeline card with a delete option
-var insertHello = function (client, errorCallback, successCallback) {
-    client
-        .mirror.timeline.insert(
-        {
-            "text": "Hello world",
-            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8081/reply",
-            "menuItems": [
-                {"action": "REPLY"},
-                {"action": "DELETE"}
-            ]
-        }
-    )
-        .withAuthClient(oauth2Client)
-        .execute(function (err, data) {
-            if (!!err)
-                errorCallback(err);
-            else
-                successCallback(data);
-        });
+var insertHello = function(client, errorCallback, successCallback) {
+	client.mirror.timeline
+			.insert(
+					{
+						"text" : "Hello, World",
+						"callbackUrl" : "https://mirrornotifications.appspot.com/forward?url=http://localhost:8081/reply",
+						"menuItems" : [ {
+							"action" : "REPLY"
+						}, {
+							"action" : "DELETE"
+						} ]
+					}).withAuthClient(oauth2Client).execute(
+					function(err, data) {
+						if (!!err)
+							errorCallback(err);
+						else
+							successCallback(data);
+					});
 };
 
-// send a simple 'hello world' timeline card with a delete option
-var insertLocation = function (client, errorCallback, successCallback) {
-    client
-        .mirror.timeline.insert(
-        {
-            "text": "Let's meet at the Hacker Dojo!",
-            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8081/reply",
-            "location": {
-                "kind": "mirror#location",
-                "latitude": 37.4028344,
-                "longitude": -122.0496017,
-                "displayName": "Hacker Dojo",
-                "address": "599 Fairchild Dr, Mountain View, CA"
-            },
-            "menuItems": [
-                {"action":"NAVIGATE"},
-                {"action": "REPLY"},
-                {"action": "DELETE"}
-            ]
-        }
-    )
-        .withAuthClient(oauth2Client)
-        .execute(function (err, data) {
-            if (!!err)
-                errorCallback(err);
-            else
-                successCallback(data);
-        });
+var listTimeline = function(client, errorCallback, successCallback) {
+	client.mirror.timeline.list().withAuthClient(oauth2Client).execute(
+			function(err, data) {
+				if (!!err)
+					errorCallback(err);
+				else
+					successCallback(data);
+			});
+};
+var grabToken = function(code, errorCallback, successCallback) {
+	oauth2Client.getToken(code, function(err, tokens) {
+		if (!!err) {
+			errorCallback(err);
+		} else {
+			console.log('tokens', tokens);
+			oauth2Client.credentials = tokens;
+			successCallback();
+		}
+	});
 };
 
-
-var insertContact = function (client, errorCallback, successCallback) {
-    client
-        .mirror.contacts.insert(
-        {
-            "id": "emil10001",
-            "displayName": "emil10001",
-            "iconUrl": "https://secure.gravatar.com/avatar/bc6e3312f288a4d00ba25500a2c8f6d9.png",
-            "priority": 7,
-            "acceptCommands": [
-                {"type": "REPLY"},
-                {"type": "POST_AN_UPDATE"},
-                {"type": "TAKE_A_NOTE"}
-            ]
-        }
-    )
-        .withAuthClient(oauth2Client)
-        .execute(function (err, data) {
-            if (!!err)
-                errorCallback(err);
-            else
-                successCallback(data);
-        });
-};
-var listTimeline = function (client, errorCallback, successCallback) {
-    client
-        .mirror.timeline.list()
-        .withAuthClient(oauth2Client)
-        .execute(function (err, data) {
-            if (!!err)
-                errorCallback(err);
-            else
-                successCallback(data);
-        });
-};
-var grabToken = function (code, errorCallback, successCallback) {
-    oauth2Client.getToken(code, function (err, tokens) {
-        if (!!err) {
-            errorCallback(err);
-        } else {
-            console.log('tokens', tokens);
-            oauth2Client.credentials = tokens;
-            successCallback();
-        }
-    });
-};
-
-app.get('/', function (req, res) {
-    if (!oauth2Client.credentials) {
-        // generates a url that allows offline access and asks permissions
-        // for Mirror API scope.
-        var url = oauth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: 'https://www.googleapis.com/auth/glass.timeline'
-        });
-        res.redirect(url);
-    } else {
-        gotToken();
-    }
-    res.write('Glass Mirror API with Node');
-    res.end();
+app.get('/', function(req, res) {
+	console.log("mark");
+	console.log("mark: " + process.env.SDFGGXE16_CLIENT_ID);
+	if (!oauth2Client.credentials) {
+		// generates a url that allows offline access and asks permissions
+		// for Mirror API scope.
+		var url = oauth2Client.generateAuthUrl({
+			access_type : 'offline',
+			scope : 'https://www.googleapis.com/auth/glass.timeline'
+		});
+		res.redirect(url);
+	} else {
+		gotToken();
+	}
+	res.write('Hello, World card sent to Glass Timeline.  SDFGGXE16 FTW');
+	res.end();
 
 });
-app.get('/oauth2callback', function (req, res) {
-    // if we're able to grab the token, redirect the user back to the main page
-    grabToken(req.query.code, failure, function () {
-        res.redirect('/');
-    });
+app.get('/oauth2callback', function(req, res) {
+	// if we're able to grab the token, redirect the user back to the main page
+	grabToken(req.query.code, failure, function() {
+		res.redirect('/');
+	});
 });
-app.post('/reply', function(req, res){
-    console.log('replied',req);
-    res.end();
+app.post('/reply', function(req, res) {
+	console.log('replied', req);
+	res.end();
 });
-app.post('/location', function(req, res){
-    console.log('location',req);
-    res.end();
+app.post('/location', function(req, res) {
+	console.log('location', req);
+	res.end();
 });
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
